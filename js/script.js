e9fa4674204195879c464b22d46802fba4aa16a2
Test Project <meharshani963@gmail.com>
2024-02-13 08:11:38 +0500

    script chanes

--- js/script.js
+++ js/script.js
@@ -1,3 +1,23 @@
+
+$(document).ready(function(){
+$('.custom1').owlCarousel({
+    loop:false,
+    margin:20,
+    dotEach:true,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:4
+        }
+    }
+});
+
 $('.owl-carousel').owlCarousel({
     loop:false,
     margin:20,
@@ -14,4 +34,6 @@ $('.owl-carousel').owlCarousel({
             items:4
         }
     }
-})
+});
+
+});
