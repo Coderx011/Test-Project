ddabf60c7c3f6aa7454bb10a4404313b299af010
Test Project <meharshani963@gmail.com>
2024-02-14 08:41:32 +0500

    slider

--- js/script.js
+++ js/script.js
@@ -1,41 +1,84 @@
 
+
+const rangeInput = document.querySelectorAll(".price-range-input input"),
+priceInput = document.querySelectorAll(".input-field input"),
+range = document.querySelector(".price-slider .price-progress-bar");
+let priceGap = 1000;
+priceInput.forEach(input =>{
+    input.addEventListener("input", e =>{
+        let minPrice = parseInt(priceInput[0].value),
+        maxPrice = parseInt(priceInput[1].value);
+        
+        if((maxPrice - minPrice >= priceGap) && maxPrice <= rangeInput[1].max){
+            if(e.target.className === "input-min"){
+                rangeInput[0].value = minPrice;
+                range.style.left = ((minPrice / rangeInput[0].max) * 100) + "%";
+            }else{
+                rangeInput[1].value = maxPrice;
+                range.style.right = 100 - (maxPrice / rangeInput[1].max) * 100 + "%";
+            }
+        }
+    });
+});
+
+rangeInput.forEach(input =>{
+    input.addEventListener("input", e =>{
+        let minVal = parseInt(rangeInput[0].value),
+        maxVal = parseInt(rangeInput[1].value);
+        if((maxVal - minVal) < priceGap){
+            if(e.target.className === "range-min"){
+                rangeInput[0].value = maxVal - priceGap
+            }else{
+                rangeInput[1].value = minVal + priceGap;
+            }
+        }else{
+            priceInput[0].value = minVal;
+            priceInput[1].value = maxVal;
+            range.style.left = ((minVal / rangeInput[0].max) * 100) + "%";
+            range.style.right = 100 - (maxVal / rangeInput[1].max) * 100 + "%";
+        }
+    });
+});
+
+
+
 $(document).ready(function(){
-$('.custom1').owlCarousel({
-    loop:true,
-    margin:20,
-    item:1,
-    dotEach:true,
-    nav:true,
-    responsive:{
-        0:{
-            items:1
-        },
-        600:{
-            items:1
-        },
-        1000:{
-            items:1
+    $('.custom1').owlCarousel({
+        loop:true,
+        margin:20,
+        item:1,
+        dotEach:true,
+        nav:true,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:1
+            },
+            1000:{
+                items:1
+            }
         }
-    }
+        
+    });
     
-});
-
-$('.owl-carousel').owlCarousel({
-    loop:false,
-    margin:20,
-    dotEach:true,
-    nav:true,
-    responsive:{
-        0:{
-            items:1
-        },
-        600:{
-            items:3
-        },
-        1000:{
-            items:4
+    $('.owl-carousel').owlCarousel({
+        loop:false,
+        margin:20,
+        dotEach:true,
+        nav:true,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:3
+            },
+            1000:{
+                items:4
+            }
         }
-    }
-});
-
-});
+    });
+    
+    });
